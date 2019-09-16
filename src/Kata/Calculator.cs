1ef342910fdd6e4f53d8230326c135943a7ad7c9
intiv <inti.velasquez@herounit.io>
2019-09-16 15:21:57 -0600

    New line delimiter passing, custom delimeters failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,7 +9,7 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(s))
                 return 0;
-            var strings = s.Split(",").Select(int.Parse);
+            var strings = s.Split(new []{",","\n"}, StringSplitOptions.None).Select(int.Parse);
             return strings.Sum();
         }
     }
