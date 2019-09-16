b33f21e14f907ec9ef67e1b5b69d8b9913466e93
intiv <inti.velasquez@herounit.io>
2019-09-16 15:37:43 -0600

    One negative passing, multiple negatives failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -17,6 +17,11 @@ namespace Kata
                 s = split.Last();
             }
             var strings = s.Split(separator, StringSplitOptions.None).Select(int.Parse);
+            var negatives = strings.Where((num) => num < 0);
+            if (negatives.Count() > 0)
+            {
+                throw new Exception("negatives not allowed: -2");
+            }
             return strings.Sum();
         }
     }
