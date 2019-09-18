b099fd5d32fa29143a53dd9e29b74b29e0a69e7d
intiv <inti.velasquez@herounit.io>
2019-09-18 14:54:46 -0600

    Multiple char custom delimiter working, multiple single delimiters failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -13,8 +13,8 @@ namespace Kata
             if (s.StartsWith("//"))
             {
                 var strings = s.Split("\n");
-                // "//;" 
-                separator = new[] {strings.First().Replace("//", "")};
+                
+                separator = new[] {strings.First().Replace("//", "").Replace("[", "").Replace("]", "")};
                 s = strings.Last();
             }
 
