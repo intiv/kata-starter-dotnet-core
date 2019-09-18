22427d8b39f59164b150d7b353dab5ce122e2a58
intiv <inti.velasquez@herounit.io>
2019-09-18 14:14:29 -0600

    One number passing, two numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,10 +1,14 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
     {
         public int Add(string s="")
         {
-            return 0;
+            if(string.IsNullOrEmpty(s))
+                return 0;
+            return Int32.Parse(s);
         }
     }
 }
