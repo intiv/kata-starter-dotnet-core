13d4ad0d3a29cf209a7e30af6213b9c94f8afb46
intiv <inti.velasquez@herounit.io>
2019-09-25 13:33:37 -0600

    Refactor methods

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Kata
@@ -9,22 +10,33 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            var separator = new []{"\n",","};
-            if(s.StartsWith("//"))
+            var numbers = GetSeparators(s);
+            HasNegatives(numbers);
+            return numbers.Sum();
+        }
+
+        static IEnumerable<int> GetSeparators(string s)
+        {
+            var separator = new[] {"\n", ","};
+            if (s.StartsWith("//"))
             {
                 var strings = s.Split("\n");
                 separator = strings.First().Replace("//", "").Replace("[", "").Split("]");
                 s = strings.Last();
             }
 
-            
-            var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse).Where(x=>x<1001);
+
+            var numbers = s.Split(separator, StringSplitOptions.None).Select(int.Parse).Where(x => x < 1001);
+            return numbers;
+        }
+
+        static void HasNegatives(IEnumerable<int> numbers)
+        {
             var negatives = numbers.Where(x => x < 0);
             if (negatives.Any())
             {
                 throw new Exception($@"Negatives not allowed: {string.Join(", ", negatives)}");
             }
-            return numbers.Sum();
         }
     }
 }
