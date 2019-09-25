fe83d1b6dca324ee3c7f4721fe8193f8b105c695
afloresmorales <afloresmorales96@gmail.com>
2019-09-25 13:32:00 -0600

    multiple custom delimiters passing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -13,7 +13,7 @@ namespace Kata
             if(s.StartsWith("//"))
             {
                 var strings = s.Split("\n");
-                separator = new []{strings.First().Replace("//", "").Replace("[", "").Replace("]", "")};
+                separator = strings.First().Replace("//", "").Replace("[", "").Split("]");
                 s = strings.Last();
             }
 
