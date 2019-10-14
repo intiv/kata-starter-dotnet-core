0631042a8a0aabcd25ec3cf5d203b8d6c2e11631
inti <inti.velasquez@acklenavenue.com>
2019-10-14 13:49:29 -0600

    One negative passing, multiple negatives failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -16,7 +16,11 @@ namespace Kata
                 separator = new[] {split.First().Replace("//", "")};
                 s = split.Last();
             }
+
             var strings = s.Split(separator, StringSplitOptions.None).Select(int.Parse);
+            var negatives = strings.Where(x => x < 0);
+            if(negatives.Any())
+                throw new Exception("negatives not allowed: -2");
             return strings.Sum();
         }
     }
