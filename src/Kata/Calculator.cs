f437c08178935db7cc476cad411d0c4ab73a9c24
afloresmorales <afloresmorales96@gmail.com>
2019-10-14 13:52:05 -0600

    multiple negatives passing, number larger than 1000 failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -20,7 +20,7 @@ namespace Kata
             var strings = s.Split(separator, StringSplitOptions.None).Select(int.Parse);
             var negatives = strings.Where(x => x < 0);
             if(negatives.Any())
-                throw new Exception("negatives not allowed: -2");
+                throw new Exception($@"negatives not allowed: {string.Join(", ",negatives)}");
             return strings.Sum();
         }
     }
