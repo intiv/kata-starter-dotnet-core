dc3c60f3859e7fa7f406dbabfa24982b6b0f3139
afloresmorales <afloresmorales96@gmail.com>
2019-10-14 13:56:01 -0600

    mutlichar delimiter passing, multiple delimiters failing.

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -13,7 +13,7 @@ namespace Kata
             if (s.StartsWith("//"))
             {
                 var split = s.Split("\n");
-                separator = new[] {split.First().Replace("//", "")};
+                separator = new[] {split.First().Replace("//", "").Replace("[","").Replace("]","")};
                 s = split.Last();
             }
 
