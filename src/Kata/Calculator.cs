1e174c4c8af609a43dffc92622fbf47bd77d8152
afloresmorales <afloresmorales96@gmail.com>
2019-10-21 13:11:30 -0600

    newline delimiter passing, custom delimiter failing.

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Kata
 {
@@ -8,7 +9,7 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            var numbers = s.Split(",").Select(int.Parse);
+            var numbers = s.Split(new []{",","\n"},StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
         }
     }
