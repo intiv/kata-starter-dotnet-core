60fb6ce404505665ff4febb8fae104c2d9d19e39
inti <inti.velasquez@acklenavenue.com>
2019-11-04 13:11:48 -0600

    Two numbers passing, unknown amoun, failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -6,7 +8,10 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            return 1;
+            var strings = s.Split(",");
+            if (strings.Length == 1)
+                return int.Parse(strings.First());
+            return int.Parse(strings.First())+int.Parse(strings.Last());
         }
     }
 }
